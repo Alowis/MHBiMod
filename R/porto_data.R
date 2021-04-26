09dee6126e94055bf3004259fe05d43fcf6181b7
unknown <alo.tilloy@gmail.com>
2021-04-26 13:35:05 +0300

    clean descriptions

--- R/porto_data.R
+++ R/porto_data.R
@@ -2,7 +2,7 @@
 #'
 #' Bivariate dataset of daily mean temperature and number of wildfires in the Porto
 #' district (Portugal) for the period 1980 to 2005. Daily mean temperature data
-#' from E-OBS (Cornes et al. (2018) <doi:10.1029/2017JD028200>
+#' from E-OBS (Cornes et al. (2018) <10.1029/2017JD028200>
 #' and wildfire data from Pereira et al. (2011) <10.5194/nhess-11-3343-2011>.
 
 #'
@@ -10,7 +10,7 @@
 #'
 #' @usage data(porto)
 #'
-#' @format A dataframe composed of two column:
+#' @format Two column dataframe:
 #' #' \itemize{
 #' \item temp2.temperature (mean daily temperature in °C)
 #' \item temp2.nb (daily number of wildfires)
